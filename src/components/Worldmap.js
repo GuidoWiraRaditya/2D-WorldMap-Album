184a556b374c0702f2afed5194d70243f00aefe8
Guido97 <guido.putro@xtramilesolutions.com.au>
2025-02-07 13:44:33 +0700

    feature: limit scroll and zoom

--- src/components/Worldmap.js
+++ src/components/Worldmap.js
@@ -55,7 +55,7 @@ export default function WorldMap() {
     }, []);
 
     return (
-        <MapContainer center={[20,0]} zoom={2} style={{ height: "100vh", width: "100vw"}} zoomControl={false}>
+        <MapContainer center={[20,0]} zoom={2} style={{ height: "100vh", width: "100vw"}} zoomControl={false} maxBounds={[[85, -180], [-85, 180]]} maxBoundsViscosity={1.0} minZoom={2}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
